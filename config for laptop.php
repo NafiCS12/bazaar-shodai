feb5ae4895fca55ea8b8da415aa7fa695e0d65bd
NafiCS12 <nafi.sabith.1993@gmail.com>
2016-08-18 22:52:20 +0600

    Add files via upload

---
+++ config for laptop.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Bazaar shodai/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Bazaar shodai/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\xampp\htdocs\Bazaar shodai/catalog/');
+define('DIR_SYSTEM', 'D:\xampp\htdocs\Bazaar shodai/system/');
+define('DIR_LANGUAGE', 'D:\xampp\htdocs\Bazaar shodai/catalog/language/');
+define('DIR_TEMPLATE', 'D:\xampp\htdocs\Bazaar shodai/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\xampp\htdocs\Bazaar shodai/system/config/');
+define('DIR_IMAGE', 'D:\xampp\htdocs\Bazaar shodai/image/');
+define('DIR_CACHE', 'D:\xampp\htdocs\Bazaar shodai/system/cache/');
+define('DIR_DOWNLOAD', 'D:\xampp\htdocs\Bazaar shodai/system/download/');
+define('DIR_UPLOAD', 'D:\xampp\htdocs\Bazaar shodai/system/upload/');
+define('DIR_MODIFICATION', 'D:\xampp\htdocs\Bazaar shodai/system/modification/');
+define('DIR_LOGS', 'D:\xampp\htdocs\Bazaar shodai/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'bazaar shodai');
+define('DB_PREFIX', 'oc_');
--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'http://localhost/Bazaar shodai/');
 define('HTTPS_SERVER', 'http://localhost/Bazaar shodai/');
 
 // DIR
-define('DIR_APPLICATION', 'E:\xampp\htdocs\Bazaar shodai/catalog/');
-define('DIR_SYSTEM', 'E:\xampp\htdocs\Bazaar shodai/system/');
-define('DIR_LANGUAGE', 'E:\xampp\htdocs\Bazaar shodai/catalog/language/');
-define('DIR_TEMPLATE', 'E:\xampp\htdocs\Bazaar shodai/catalog/view/theme/');
-define('DIR_CONFIG', 'E:\xampp\htdocs\Bazaar shodai/system/config/');
-define('DIR_IMAGE', 'E:\xampp\htdocs\Bazaar shodai/image/');
-define('DIR_CACHE', 'E:\xampp\htdocs\Bazaar shodai/system/cache/');
-define('DIR_DOWNLOAD', 'E:\xampp\htdocs\Bazaar shodai/system/download/');
-define('DIR_UPLOAD', 'E:\xampp\htdocs\Bazaar shodai/system/upload/');
-define('DIR_MODIFICATION', 'E:\xampp\htdocs\Bazaar shodai/system/modification/');
-define('DIR_LOGS', 'E:\xampp\htdocs\Bazaar shodai/system/logs/');
+define('DIR_APPLICATION', 'C:\xampp\htdocs\Bazaar shodai/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\Bazaar shodai/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\Bazaar shodai/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\Bazaar shodai/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\Bazaar shodai/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\Bazaar shodai/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\Bazaar shodai/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\Bazaar shodai/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\Bazaar shodai/system/upload/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\Bazaar shodai/system/modification/');
+define('DIR_LOGS', 'C:\xampp\htdocs\Bazaar shodai/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
